8eca3f02b8e3306839b130d9d6d647a4abb10739
Shreya Ray <161076300+forgottenflowers@users.noreply.github.com>
2025-09-23 16:06:59 +0200

    Update 05_convert_imageJ_tif_to_stack.py

--- 05_convert_imageJ_tif_to_stack.py
+++ 05_convert_imageJ_tif_to_stack.py
@@ -5,9 +5,10 @@ import logging
 from ij import IJ
 
 # Define folders
-in_path = "T:/tomo/Shreya/2505_soleil_recon_FijiTIFfiles"
-out_path = "T:/tomo/Shreya/2505_soleil_recon_multipleTiffs"
-log_path = "C:/Users/sysgen/Desktop/multipletiff_log_file.txt"
+in_path = "E:/Shreya/2505_soleil_recon_hyperstacks"
+out_path = "E:/Shreya/"
+log_path = "C:/Users/shra13/Desktop/multipletiff_log_file.txt"
+# log_path = "C:/Users/sysgen/Desktop/multipletiff_log_file.txt"
 
 # Save log
 logging.basicConfig(filename=log_path, level=logging.INFO, format="%(asctime)s - %(message)s")
@@ -19,7 +20,8 @@ tif_files = glob.glob(os.path.join(in_path, "*.tif"))
 for tif_path in tif_files:
 
     start_time = time.time()
-    print("working...")
+    print("working on: " + tif_path)
+    logging.info("working on: %s", tif_path)
     
     # Get output directory name
     base = os.path.basename(tif_path)
@@ -27,7 +29,8 @@ for tif_path in tif_files:
     
     # Create output directory in the output path
     out_dir = os.path.join(out_path, foldername)
-    os.makedirs(out_dir, exist_ok=True)                 # won't fail if it already exists
+    if not os.path.exists(out_dir):                     # Python 3.2: os.makedirs(out_dir, exist_ok=True)
+        os.makedirs(out_dir)
     
     # Open the image (ImagePlus object)
     imp = IJ.openImage(tif_path)
@@ -43,9 +46,14 @@ for tif_path in tif_files:
     logging.info("%s    Time elapsed: %.2f seconds", out_dir, elapsed)
 
 print("done...")
+logging.info("All files processed.")
 
 # ----------------------------------------------------------------------------------------------
 # open windows command line: Press the Windows key + r. In the Run box, type cmd, and then click Ok. This opens the Command Prompt window.
+# Ctr+C to interrupt code and end it
+
+# cd C:\opt\Fiji.app
+# ImageJ-win64.exe --ij2 --headless --console --run "C:\Users\shra13\Desktop\testcode.py"
+
 # cd C:\opt\Fiji_Interdent_Oleksandra\Fiji.app
 # ImageJ-win64_Interdent.exe --ij2 --headless --console --run "C:\Users\sysgen\Desktop\try.py"
-# Ctr+C to interrupt code and end it
